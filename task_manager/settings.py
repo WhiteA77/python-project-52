c9ef0bf6480358ec0f5a2f4694d4d5d50ce64300
WhiteA <crazy032@mail.ru>
2025-10-16 14:24:12 +0300

    Серверный рендеринг . Шаг 2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +70,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello from Task Manager!")
+    return render(request, "index.html")
