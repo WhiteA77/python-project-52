b9ebcf9a3e93e9b76fb9adb7c92801b8930068d8
WhiteA <crazy032@mail.ru>
2025-10-23 10:30:47 +0300

    Fix failing user update test

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,6 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 
 class UserRegisterForm(UserCreationForm):
@@ -23,10 +23,40 @@ class UserRegisterForm(UserCreationForm):
 class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True, label="Имя")
     last_name = forms.CharField(max_length=150, required=True, label="Фамилия")
+    password1 = forms.CharField(
+        label="Пароль",
+        strip=False,
+        widget=forms.PasswordInput,
+        required=False,
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        strip=False,
+        widget=forms.PasswordInput,
+        required=False,
+    )
 
     class Meta:
         model = User
         fields = ("first_name", "last_name", "username")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        if password1 or password2:
+            if password1 != password2:
+                raise forms.ValidationError("Введенные пароли не совпадают")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
 
 
 class UserLoginForm(AuthenticationForm):
